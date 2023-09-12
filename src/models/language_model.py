9bd448a184bc827b764c6f72c5dd427432c9ccb8
andreresende36 <andreresende36@gmail.com>
2023-09-12 17:23:02 -0300

    feature 2 and 3: to_dict and list_dicts done

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -4,16 +4,25 @@ from database.db import db
 
 # Req. 1
 class LanguageModel(AbstractModel):
-    _collection = db['languages']
+    _collection = db["languages"]
 
     def __init__(self, data: dict[str, str]):
         super().__init__(data)
 
     # Req. 2
     def to_dict(self):
-        raise NotImplementedError
+        return {
+            "name": self.data.get("name", ""),
+            "acronym": self.data.get("acronym", ""),
+        }
 
     # Req. 3
     @classmethod
     def list_dicts(cls):
-        raise NotImplementedError
+        languages = cls._collection.find()
+        language_list = [
+            {"name": language["name"], "acronym": language["acronym"]}
+            for language in languages
+        ]
+        print("aquiiiiiiii", language_list)
+        return language_list
