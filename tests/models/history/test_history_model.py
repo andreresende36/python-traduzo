29c29d648c6bd1d218242d9ed0a3c91d25418965
andreresende36 <andreresende36@gmail.com>
2023-09-12 20:30:32 -0300

    feature 7: test history_model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,22 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_list_as_json():
+    translations = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    history_json = json.loads(HistoryModel.list_as_json())
+    for i, translation in enumerate(translations):
+        del history_json[i]["_id"]
+        assert history_json[i] == translation
