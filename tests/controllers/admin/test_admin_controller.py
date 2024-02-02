f84327693d663c58054b9bf70f298fb726949ae8
andreresende36 <andreresende36@gmail.com>
2024-02-02 10:32:43 -0300

    final project

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,54 +1,35 @@
-# from src.models.user_model import UserModel
-# from src.models.history_model import HistoryModel
-# from src.app import app
-# from flask.testing import FlaskClient
-# import json
-# from bson import ObjectId
+from bson import ObjectId
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
+from src.database.db import db
 
 
-# # Req. 9
-# def test_history_delete(app_test: FlaskClient):
-#     # Cria um usuário para autenticação
-#     user_data = {
-#         "name": "admin_user",
-#         "password": "admin_password",
-#     }
-#     user = UserModel(user_data)
-#     user.save()
+# Req. 9
+def test_history_delete(app_test):
+    mock_data = {"name": "admin", "password": "123456", "token": "123456"}
+    user = UserModel(mock_data)
+    user.save()
 
-#     # Cria um registro de histórico para exclusão
-#     history_data = {
-#         "text_to_translate": "Aqui tem um texto para teste",
-#         "translate_from": "en",
-#         "translate_to": "pt",
-#     }
-#     history = HistoryModel(history_data)
-#     history.save()
+    mock_history_data = {
+        "text-to-translate": "Hello, I like videogame",
+        "translate-from": "en",
+        "translate-to": "pt",
+    }
+    history = HistoryModel(mock_history_data)
+    history.save()
 
-#     # Autentica o usuário e obtém o token
-#     response = app_test.post(
-#         "/auth",
-#         data={
-#             "name": "admin_user",
-#             "password": "admin_password",
-#         },
-#     )
-#     assert response.status_code == 200
-#     auth_token = json.loads(response.data)["token"]
-
-#     # Obtém o ID do registro de histórico
-#     history_id = str(history["_id"])
-
-#     # Faz uma solicitação DELETE para excluir o registro
-#     response = app_test.delete(
-#         f"/admin/history/{history_id}",
-#         headers={
-#             "Authorization": auth_token,
-#             "User": "admin_user",
-#         },
-#     )
-#     assert response.status_code == 204
-
-#     # Verifica se o registro foi excluído do banco de dados
-#     deleted_history = HistoryModel.find_one({"_id": ObjectId(history_id)})
-#     assert deleted_history is None
+    assert (
+        db.get_collection("history").find_one({"_id": history.data["_id"]})
+        is not None
+    )
+    app_test.delete(
+        f"/admin/history/{ObjectId(history.data['_id'])}",
+        headers={
+            "Authorization": user.data["token"],
+            "User": user.data["name"],
+        },
+    )
+    assert (
+        db.get_collection("history").find_one({"_id": history.data["_id"]})
+        is None
+    )
